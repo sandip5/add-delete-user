28977feaccb26c1a5e1eb04dab82373614aec4db
sandip5 <singhsandeep.singh97@gmail.com>
2021-11-07 21:36:47 +0530

    Added User List

---
+++ src/components/Users/UserList.js
@@ -0,0 +1,14 @@
+import Card from '../UI/Card';
+import classes from './UserList.module.css';
+
+const UserList = props => {
+    return (
+        <Card className={classes.users}>
+            <ul>
+                {props.users.map(user => <li>{user.name} ({user.age} Years Old)</li>)}
+            </ul>
+        </Card>
+    );
+};
+
+export default UserList;
